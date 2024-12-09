a7371b108bd82c0a9f4ba87026fbdaaa79ea3714
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-12-09 16:13:12 +0530

    Solve Part 1

--- lib/advent/year2024/day09.ex
+++ lib/advent/year2024/day09.ex
@@ -44,16 +44,39 @@ defmodule Advent.Year2024.Day09 do
       %Disk{blocks: compacted_blocks, total_files: disk.total_files, total_blocks: total_blocks}
     end
 
-    defp compact([], _), do: []
-    defp compact(_, []), do: []
-    defp compact([{:file, a, b, c} | rest], other), do: [{:file, a, b, c} | compact(rest, other)]
-    defp compact(other, [{:space, _, _} | rest]), do: compact(other, rest)
+    defp compact([{:file, fid, start1, fsz} | rest1], [{:file, _, start2, fsz2} | _] = other) do
+      cond do
+        start1 > start2 -> []
+        start1 == start2 -> [{:file, fid, start2, fsz2}]
+        true -> [{:file, fid, start1, fsz} | compact(rest1, other)]
+      end
+    end
+
+    defp compact([{:space, start1, _} | _] = spaces, [{:space, start2, _} | rest2]) do
+      cond do
+        start1 > start2 ->
+          []
+
+        true ->
+          compact(spaces, rest2)
+      end
+    end
+
+    defp compact([{:file, fid, file_start, fsz} | rest1], [{:space, space_start, _} | rest2]) do
+      cond do
+        file_start > space_start -> []
+        true -> [{:file, fid, file_start, fsz} | compact(rest1, rest2)]
+      end
+    end
 
     defp compact(
            [{:space, space_start, space_size} | rest_spaces],
            [{:file, file_id, file_start, file_size} | rest_files]
          ) do
       cond do
+        space_start > file_start ->
+          []
+
         space_size == file_size ->
           [{:file, file_id, space_start, file_size} | compact(rest_spaces, rest_files)]
 
@@ -72,7 +95,7 @@ defmodule Advent.Year2024.Day09 do
     disk = Disk.parse(args)
     compacted_disk = Disk.compactify(disk)
 
-    IO.inspect(compacted_disk)
+    # IO.inspect(compacted_disk)
 
     # [{:space, _, space_size}] =
     #   compacted_disk.blocks
@@ -94,35 +117,35 @@ defmodule Advent.Year2024.Day09 do
     #   |> Enum.sum()
     #
     # IO.inspect({space_before, space_after}, label: "testing equality")
-
-    original =
-      disk.blocks
-      |> Enum.filter(fn
-        {:space, _, _} -> false
-        {:file, _, _, _} -> true
-      end)
-      |> Enum.reduce(%{}, fn {:file, id, _, sz}, acc ->
-        Map.update(acc, id, sz, fn old -> old + sz end)
-      end)
-
-    new =
-      compacted_disk.blocks
-      |> Enum.filter(fn
-        {:space, _, _} -> false
-        {:file, _, _, _} -> true
-      end)
-      |> Enum.reduce(%{}, fn {:file, id, _, sz}, acc ->
-        Map.update(acc, id, sz, fn old -> old + sz end)
-      end)
-
-    discrepencies =
-      for {id, sz} <- original do
-        nsz = Map.get(new, id)
-        if nsz != sz, do: {id, sz, nsz}, else: nil
-      end
-      |> Enum.filter(& &1)
-
-    IO.inspect(discrepencies, label: "discrepencies")
+    #
+    # original =
+    #   disk.blocks
+    #   |> Enum.filter(fn
+    #     {:space, _, _} -> false
+    #     {:file, _, _, _} -> true
+    #   end)
+    #   |> Enum.reduce(%{}, fn {:file, id, _, sz}, acc ->
+    #     Map.update(acc, id, sz, fn old -> old + sz end)
+    #   end)
+    #
+    # new =
+    #   compacted_disk.blocks
+    #   |> Enum.filter(fn
+    #     {:space, _, _} -> false
+    #     {:file, _, _, _} -> true
+    #   end)
+    #   |> Enum.reduce(%{}, fn {:file, id, _, sz}, acc ->
+    #     Map.update(acc, id, sz, fn old -> old + sz end)
+    #   end)
+    #
+    # discrepencies =
+    #   for {id, sz} <- original do
+    #     nsz = Map.get(new, id)
+    #     if nsz != sz, do: {id, sz, nsz}, else: nil
+    #   end
+    #   |> Enum.filter(& &1)
+    #
+    # IO.inspect(discrepencies, label: "discrepencies")
 
     compacted_disk.blocks
     |> Enum.filter(fn block ->
@@ -132,12 +155,8 @@ defmodule Advent.Year2024.Day09 do
       end
     end)
     |> Enum.map(fn {:file, file_id, start, size} ->
-      for m <- start..(start + size - 1) do
-        m * file_id
-      end
-      |> Enum.sum()
+      size * file_id * start + file_id * div(size * (size - 1), 2)
     end)
-    |> IO.inspect()
     |> Enum.sum()
   end
 
