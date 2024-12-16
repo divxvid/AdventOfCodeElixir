959cbed3558c2c9216b3808a47c85eab85b9b32b
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-12-16 20:48:17 +0530

    Solve Day 16

--- lib/advent/year2024/day16.ex
+++ lib/advent/year2024/day16.ex
@@ -8,67 +8,116 @@ defmodule Advent.Year2024.Day16 do
   def part1(args) do
     %Grid{elements: grid} = Grid.new(args)
 
-    {src_r, src_c} = get_src(grid)
+    {{src_r, src_c}, {dest_r, dest_c}} = get_src_and_dest(grid)
+    min_points_seen = find_min_points(grid, src_r, src_c)
+
+    min_points_seen[{dest_r, dest_c}]
+  end
+
+  def part2(args) do
+    %Grid{elements: grid} = Grid.new(args)
+
+    {{src_r, src_c}, {dest_r, dest_c}} = get_src_and_dest(grid)
+    min_points_seen = find_min_points(grid, src_r, src_c)
+
+    min_points = min_points_seen[{dest_r, dest_c}]
+
+    best_spots = MapSet.new() |> MapSet.put({dest_r, dest_c})
+
+    [:north, :south, :east, :west]
+    |> Enum.reduce(best_spots, fn dir, best_spots ->
+      find_best_spots(min_points_seen, dest_r, dest_c, min_points, dir, best_spots)
+    end)
+    |> MapSet.size()
+  end
+
+  defp find_best_spots(min_points_seen, r, c, points, dir, best_spots) do
+    generate_neighbors(r, c, points, dir, -1)
+    |> Enum.reduce(best_spots, fn {npts, {nr, nc, ndir}}, best_spots ->
+      value = Map.fetch(min_points_seen, {nr, nc})
+
+      case value do
+        {:ok, ^npts} ->
+          best_spots = MapSet.put(best_spots, {nr, nc})
+          find_best_spots(min_points_seen, nr, nc, npts, ndir, best_spots)
+
+        {:ok, actual_pts} ->
+          delta = npts - actual_pts
+
+          if delta == 1000 do
+            best_spots = MapSet.put(best_spots, {nr, nc})
+            find_best_spots(min_points_seen, nr, nc, npts, ndir, best_spots)
+          else
+            best_spots
+          end
+
+        _ ->
+          best_spots
+      end
+    end)
+  end
+
+  defp find_min_points(grid, src_r, src_c) do
     heap = Heap.new(fn {k1, _}, {k2, _} -> k1 <= k2 end)
-    min_points_seen = Map.new()
 
     # inserting the source node with 0 points
     heap = Heap.push(heap, {0, {src_r, src_c, :east}})
 
     Stream.iterate(1, & &1)
-    |> Enum.reduce_while({heap, min_points_seen}, fn _, {heap, min_points_seen} ->
-      if Heap.empty?(heap), do: raise("Heap got empty!")
-      {points, {r, c, dir}} = Heap.root(heap)
-      heap = Heap.pop(heap)
-
-      if Map.get(min_points_seen, {r, c, dir}, 1_000_000_000_000) < points do
-        {:cont, {heap, min_points_seen}}
+    |> Enum.reduce_while({heap, Map.new()}, fn _, {heap, min_points_seen} ->
+      if Heap.empty?(heap) do
+        {:halt, min_points_seen}
       else
-        node_type = Map.get(grid, {r, c})
+        {points, {r, c, dir}} = Heap.root(heap)
+        heap = Heap.pop(heap)
 
-        case node_type do
-          "#" ->
-            {:cont, {heap, min_points_seen}}
+        if Map.get(min_points_seen, {r, c}, 1_000_000_000_000) < points do
+          {:cont, {heap, min_points_seen}}
+        else
+          node_type = Map.get(grid, {r, c})
 
-          "E" ->
-            {:halt, points}
+          case node_type do
+            "#" ->
+              {:cont, {heap, min_points_seen}}
 
-          _ ->
-            min_points_seen = Map.put(min_points_seen, {r, c, dir}, points)
+            "E" ->
+              min_points_seen = Map.put(min_points_seen, {r, c}, points)
+              {:cont, {heap, min_points_seen}}
 
-            heap =
-              generate_neighbors(r, c, points, dir)
-              |> Enum.reduce(heap, fn e, heap -> Heap.push(heap, e) end)
+            _ ->
+              min_points_seen = Map.put(min_points_seen, {r, c}, points)
 
-            {:cont, {heap, min_points_seen}}
+              heap =
+                generate_neighbors(r, c, points, dir)
+                |> Enum.reduce(heap, fn e, heap -> Heap.push(heap, e) end)
+
+              {:cont, {heap, min_points_seen}}
+          end
         end
       end
     end)
   end
 
-  def part2(args) do
-    args
-  end
-
-  defp generate_neighbors(r, c, points, dir) do
+  defp generate_neighbors(r, c, points, dir, mult \\ 1) do
     index = Map.get(@index, dir)
     left_index = rem(index - 1 + 4, 4)
     right_index = rem(index + 1, 4)
 
     # IMPORTANT: I'm adding 1000+1 to accomodate for turn + movement to the next cell
     [
-      {elem(@deltas, index), points + 1, dir},
-      {elem(@deltas, left_index), points + 1001, Map.get(@reverse_index, left_index)},
-      {elem(@deltas, right_index), points + 1001, Map.get(@reverse_index, right_index)}
+      {elem(@deltas, index), points + 1 * mult, dir},
+      {elem(@deltas, left_index), points + 1001 * mult, Map.get(@reverse_index, left_index)},
+      {elem(@deltas, right_index), points + 1001 * mult, Map.get(@reverse_index, right_index)}
     ]
     |> Enum.map(fn {{dx, dy}, pts, dir} -> {pts, {r + dx, c + dy, dir}} end)
   end
 
-  defp get_src(grid) do
-    Enum.reduce_while(grid, nil, fn {{r, c}, v}, nil ->
+  defp get_src_and_dest(grid) do
+    Enum.reduce(grid, {nil, nil}, fn {{r, c}, v}, {src, dest} ->
       case v do
-        "S" -> {:halt, {r, c}}
-        _ -> {:cont, nil}
+        "S" -> {{r, c}, dest}
+        "E" -> {src, {r, c}}
+        _ -> {src, dest}
       end
     end)
   end
